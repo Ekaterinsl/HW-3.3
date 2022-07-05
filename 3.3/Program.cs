39909e77c653f1fe17f060e7fc8e9d5b4f37dd7f
Apelsin <Apelsin@DESKTOP-I9KKGKE>
2022-07-05 22:10:09 +0400

    Добавьте файлы проекта.

---
+++ 3.3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+        {
+            
+            int n;
+            int i = 2;
+            bool prostoe = true;
+
+            Console.WriteLine("Введите число  ");
+            n = int.Parse(Console.ReadLine());
+           
+            while (i <= n - 1)
+            {
+                i++;
+                if (n % i == 0)
+                {
+                    prostoe = false;
+                    break;
+                }
+            }
+            if (prostoe)
+            {
+                Console.WriteLine("Число простое");
+            }
+            else
+            {
+                Console.WriteLine("Число не простое");
+            }
+            Console.ReadKey();
+        }
+           
+            
+  
+
+      
+    }
+}
